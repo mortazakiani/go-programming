0e48fdf59402461843226ae442e5c23d12612de6
morteza <mortazakianitadi@gmail.com>
2025-01-24 16:44:19 +0330

    strings

---
+++ 02-datatype/02-02.stirngs/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("hi" , "morteza")
+	fmt.Print("hi" , "morteza")
+	fmt.Println("hi" , "morteza")
+	fmt.Println("hi" , "morteza")
+	fmt.Printf("hiiii %s %d %t %f %v test\n","morteza",2,false,1.2,"morteza") //v for value and for so much more visit https://pkg.go.dev/fmt
+
+	println("-----------------------------")
+
+	a := "test1"
+	b := "t"
+	 
+	println(a+b)
+}
