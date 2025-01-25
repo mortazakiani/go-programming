0cc22910941f47c07b284ce789d4faace23f2b8a
morteza <mortazakianitadi@gmail.com>
2025-01-25 22:48:17 +0330

    for loops

---
+++ 07-loops/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	//for  strcture
+	for i := 0; i < 5; i++ {
+		if i > 3 {
+			break//break  from loop
+		}
+		fmt.Println(i)
+	}
+	for i := 0; i < 5; i++ {
+		if i == 4 {
+			continue//except from this 
+		}
+		fmt.Println(i)
+	}
+
+
+	for i := 1; i  > 0; i++ {
+		fmt.Println("infinit")
+	}
+}
