6a3250aa6d7d44b77efda860e8b56cd21ba87864
morteza <mortazakianitadi@gmail.com>
2025-01-27 00:39:18 +0330

    arrayes

--- 09-arrays/main.go
+++ 09-arrays/main.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var a []int
-	fmt.Println(a)
+	var a [5]int//create empty arraye with 5 elemnts 
+	b := [4]int{5,6,7,8}//create a arraye with 4 elemnt (with value)
+	c := [...]int {10,20,30,40,50}//create array without specific  lenght of arraye 
+	fmt.Println(b,a,c)
+	fmt.Println("lenght a is : ",len(a))//get  lenght of arraye
+	for i ,v := range b {// navigate  in arraye to return  value and index
+		fmt.Println(i,v)
+	}
+	for _ ,v := range c {//navigate  in arraye to return just value
+		fmt.Println(v)
+	}
+	e := [3][2]string{
+		{"a","b"},
+		{"c","d"},
+		{"e","f"},
+
+	}
+	fmt.Println(e)
 }
